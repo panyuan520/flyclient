c775aefacf6c14d2825a654ff1c3a0194826d09b
376105482@qq.com <376105482@qq.com>
2017-10-25 10:06:53 +0800

    udpate

--- flyclient.py
+++ flyclient.py
@@ -6,15 +6,17 @@
 * email: 376105482@qq.com
 '''
 import re
+import sys
+sys.path.append('../')
 import traceback
 import time
 import socket
 import msgpack
 from io import BytesIO
-from itertools import imap
+from quant_backend.models import *
 
 
-class FlyCache(object):
+class FlyClient(object):
   
     def __init__(self, host="localhost", port=9999, timeout=30):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,8 +46,10 @@ class FlyCache(object):
                         data += buffer
                         self.read_length += len(buffer)
 
+
         except Exception as e:
             print traceback.print_exc()
+
 
     def get(self, key):
         key = "G{}".format(key)
@@ -56,12 +60,12 @@ class FlyCache(object):
         return self.command(key)
 
     def save(self, key, data):
-        key = "S{}\t\n".format(key)
+        key = "S{}\r\t".format(key)
         byte = bytearray()
         byte.extend(key)
         data = msgpack.packb(data)
         byte.extend(data)
-        return self.command(key)
+        return self.command(byte)
 
     def format(self, col):
         for key, forkey in {'=':'?=?', '>':'?>?', '>=':'?>=?', '<':'?<?', '<=':'?<=?'}.iteritems():
@@ -78,11 +82,10 @@ class FlyCache(object):
         	key += "|w{}".format(self.format(where))
         if order:
         	key += "|o{}".format(order.replace("=", "?"))
-
         return self.command(key)
     
     def command(self, key):
-        key += "\r\n"
+        key += "\r\r\r\n"
         self.buffer.seek(0)
         self.buffer.truncate()
         self.read_length = 0
@@ -93,12 +96,36 @@ class FlyCache(object):
 
 
 if __name__ == '__main__':
-    cache = FlyCache()
+    cache = FlyClient()
+    '''
+    df = pd.read_sql("select *  from valuation_201709 where date='2017-09-08'", engine)
+    df = df.fillna(1) 
+    del df['date']
+    del df['create_time']
+    del df['update_time']
+    del df['ipo_date']
+    #df = df.tail(1200)
+    #print list(df['pe'])
+    #df = df.head(32)
+    plist =  df.to_dict(orient='list')
+    '''
     now = time.time()
+    #print plist
+    
+    #plist = {'pe':[1,2,3,3,4,5,3,1,8],'pb':[2,3,4,5,6,7,1,1,1]}
+    
+    #print "debug:", cache.save("valuation_2017-01-07", plist)
+    
+    #data = cache.get("valuation_2017-01-07")
+    #for i in xrange(10000):
+    print cache.filter("valuation_2017-01-07", "pb", "pb<100,pb>1", "pb=1")
+    #print len(data['pe'])
     #print cache.save("test", [1,2,3,4])
     #"Fspe,pb|fvaluation_2017-01-02|wpe?>=?0,pb?<=?4|ope?0\n"
-    for i in range(10000):
+    '''
+    for i in range(1000000):
         #cache.get("test")
-        #cache.get("valuation_2017-08-20")
-        cache.filter("valuation_2017-08-20", "pe,pb", "pe>=0", "pe=1")
+        data = cache.get("valuation_2017-08-25")
+        #data = cache.filter("valuation_2017-08-25", "pe", "pe>1", "pe=1")
+    '''
     print time.time() - now
